128b0774e7999f482d9d11e16657bb2aede661d0
mgrooms <mgrooms@1bb156eb-19af-db11-b919-0014c23ffdf2>
2007-09-15 21:46:25 +0000

    Branch for 2.0.1 release.

---
+++ release-2.0.1/source/ikea/ikea.h
@@ -0,0 +1,46 @@
+#ifndef _IKEA_H_
+#define _IKEA_H_
+
+#include <pwd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <qfiledialog.h>
+#include <qlineedit.h>
+#include <qmessagebox.h>
+#include <qprocess.h>
+#include <qiconview.h>
+#include <qdir.h>
+
+#include "config.h"
+#include "root.h"
+#include "site.h"
+#include "topology.h"
+#include "about.h"
+
+typedef class _IKEA
+{
+	protected:
+
+	QString sites;
+	QString certs;
+
+	root *	r;
+
+	public:
+
+	_IKEA();
+	~_IKEA();
+
+	const char * site_path();
+	const char * cert_path();
+
+	bool init( root * setr );
+
+}IKEA;
+
+extern IKEA ikea;
+
+#endif
